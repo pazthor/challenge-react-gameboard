9529cae791676c468ba65149375d2a8811a128d7
Julio <pastor@indava.com>
2020-04-22 00:43:23 -0500

    Add initial components for the game

---
+++ src/components/LoginComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LoginComponent = () => {
+  
+  return (
+    <>
+    <div>
+      <span>Iniciar sesión o login</span>
+      <input></input>
+      <button> iniciar sesión</button>
+    </div>
+    </>
+  )
+}
+
+export default LoginComponent;
