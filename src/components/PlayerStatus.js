4578bb5aa8abed1790a0ed59234ebe98cf0f7496
Julio <pastor@indava.com>
2020-04-23 14:47:51 -0500

    Add props to PlayerStatus

--- src/components/PlayerStatus.js
+++ src/components/PlayerStatus.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const PlayerStatus = ({...}) => {
-  
-}
+const PlayerStatus = ({ name, hPMax, healtPoints, shield }) => {
+  return (
+    <>
+      <div>
+        <div>
+          <h3> Name: {name} </h3>
+        </div>
+        <div>
+          <span>
+            HP: {healtPoints}/{hPMax}
+          </span>
+          <span>Shiled: {shield}</span>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default PlayerStatus;
