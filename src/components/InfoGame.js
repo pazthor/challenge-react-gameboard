3ef7cd053674758337cb2ca71821152c78bad6fa
Julio <pastor@indava.com>
2020-04-23 18:37:16 -0500

    add store and dispatch

--- src/components/InfoGame.js
+++ src/components/InfoGame.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {store} from '../store';
 
-const InfoGame = () => {
+const InfoGame = ({current, past, left}) => {
+const context = useContext(store)
+const  {dispatch} = context;
+
+dispatch({ type: 'action description' })
   return (
     <>
     <h1>Turns</h1>
     <div>
-      <div><h4>CURRENT:</h4> <h2>12</h2></div>
-      <div><h4>PAST:</h4> <h2>12</h2></div>
-      <div><h4>LEFT:</h4> <h2>12</h2></div>
+      <div><h4>CURRENT:</h4> <h2>{current}</h2></div>
+      <div><h4>PAST:</h4> <h2>{past}</h2></div>
+      <div><h4>LEFT:</h4> <h2>{left}</h2></div>
     </div>
     <button>END TURN</button>
     </>
