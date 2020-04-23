e9d5b91504c4ca8e74870982dd35c84444a7a5ff
Julio <pastor@indava.com>
2020-04-23 14:11:44 -0500

    Add files Player  and PlayerImage

---
+++ src/components/Players.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Player from './Player';
+
+const Players = () => {
+
+  return(
+    <>
+    <Player type= {1} />
+    <Player type={0}/>
+    </>
+  )
+}
+
+export default Players;
