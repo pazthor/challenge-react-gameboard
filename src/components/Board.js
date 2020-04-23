65a1eb95e0245c794411b974867568d4cd2c265b
Julio <pastor@indava.com>
2020-04-23 18:36:34 -0500

    Update BoardComponent

---
+++ src/components/Board.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import InfoGame from './InfoGame';
+import Players from './Players';
+import Cards from './Cards';
+
+const BoardComponent = () => {
+
+  return (
+    <>
+    <InfoGame />
+    <Players />
+    <Cards />
+
+    </>
+  )  
+}
+
+export default BoardComponent;
