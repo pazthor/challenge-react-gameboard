c8ded3ef27460589c794f8ff0aa59d763ac4d0e9
Julio <pastor@indava.com>
2020-04-23 14:12:03 -0500

    Add Info Game component

---
+++ src/components/InfoGame.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const InfoGame = () => {
+  return (
+    <>
+    <h1>Turns</h1>
+    <div>
+      <div><h4>CURRENT:</h4> <h2>12</h2></div>
+      <div><h4>PAST:</h4> <h2>12</h2></div>
+      <div><h4>LEFT:</h4> <h2>12</h2></div>
+    </div>
+    <button>END TURN</button>
+    </>
+  )
+}
+
+export default InfoGame;
