fd05a18c0dedbde6c2fd74c5792e3da2668dd2b0
Julio <pastor@indava.com>
2020-04-24 02:33:45 -0500

    add logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,18 +1,47 @@
-import React from 'react'
-import InfoGame from './InfoGame';
-import Players from './Players';
-import Cards from './Cards';
+import React, { useState, useEffect } from "react";
+import InfoGame from "./InfoGame";
+import Players from "./Players";
+import Cards from "./Cards";
+import ModalEndGame from "./base/Modal";
 
-const BoardComponent = () => {
+const BoardComponent = ({
+  id,
+  currentTurn,
+  maxTurns,
+  turnsLeft,
+  monsterHp,
+  playerHp,
+  onReset
+}) => {
+  const [endGame, setEndGame] = useState(false);
+
+
+  useEffect(() => {
+    const hasHpMonster = monsterHp > 0;
+    const hasHpPlayer = playerHp > 0;
+    const hastTurns = currentTurn < maxTurns;
+    const isGameOver = !(hasHpMonster && hasHpPlayer && hastTurns);
+    setEndGame(isGameOver);
+  }, [monsterHp, playerHp, currentTurn]);
 
   return (
     <>
-    <InfoGame />
-    <Players />
-    <Cards />
-
+      <InfoGame
+        current={currentTurn}
+        past={turnsLeft}
+        left={maxTurns - currentTurn}
+        maxTurn={maxTurns}
+        gameId={id}
+      />
+      <Players gameId={id} />
+      <Cards />
+      <ModalEndGame
+        show={endGame}
+        closeModal={()=> {setEndGame(false); onReset()} }
+        isWinner={!monsterHp && currentTurn < maxTurns}
+      />
     </>
-  )  
-}
+  );
+};
 
 export default BoardComponent;
