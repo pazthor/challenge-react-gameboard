070a0985e5bd4fd19278135fac09acc11c8e36ae
Julio <pastor@indava.com>
2020-04-24 02:31:54 -0500

    add monsterEffect and cardSelected

--- src/store.js
+++ src/store.js
@@ -5,7 +5,9 @@ const initialState = {
   player: {},
   monster: {},
   gameInfo: {},
-  cardList: {},
+  cardList: [],
+  monsterEffect: {},
+  cardSelected: false,
 };
 const store = createContext(initialState);
 const { Provider } = store;
@@ -14,6 +16,8 @@ const actions = {
   SET_MONSTER: "SET_MONSTER",
   SET_GAME: "SET_GAME",
   SET_CARDLIST: "SET_CARDLIST",
+  SET_MONSTEREFFECT: "SET_MONSTEREFFECT",
+  SET_CARDSELECTED: "SET_CARDSELECTED",
   RESET: "RESET",
 };
 
@@ -27,6 +31,10 @@ function reducer(state, action) {
       return { ...state, gameInfo: action.value };
     case actions.SET_CARDLIST:
       return { ...state, cardList: action.value };
+    case actions.SET_MONSTEREFFECT:
+      return { ...state, monsterEffect: action.value };
+    case actions.SET_CARDSELECTED:
+      return { ...state, cardSelected: action.value };
     case actions.RESET:
       return { ...state, ...initialState };
     default:
@@ -39,17 +47,27 @@ const StateProvider = ({ children }) => {
     player: state.player,
     monster: state.monster,
     gameInfo: state.gameInfo,
+    cardList: state.cardList,
+    monsterEffect: state.monsterEffect,
+    cardSelected: state.cardSelected,
+
     setPlayer: (value) => {
       dispatch({ type: actions.SET_PLAYER, value });
     },
     setMonster: (value) => {
       dispatch({ type: actions.SET_MONSTER, value });
     },
+    setMonsterEffect: (value) => {
+      dispatch({ type: actions.SET_MONSTEREFFECT, value });
+    },
     setGame: (value) => {
       dispatch({ type: actions.SET_GAME, value });
     },
     setCardList: (value) => {
       dispatch({ type: actions.SET_CARDLIST, value });
+    },
+    setCardSelected: (value) => {
+      dispatch({ type: actions.SET_CARDSELECTED, value });
     },
     reset: () => {
       dispatch({ type: actions.RESET });
