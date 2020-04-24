b3611ab56b00e1dca912ba67dab31585771c0164
Julio <pastor@indava.com>
2020-04-24 02:34:16 -0500

    add props

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,31 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import LoginComponent from "./LoginComponent";
 import BoardComponent from "./Board";
+import {store} from "../store";
 
-function App() {  
-  const [isLoginState, setIsLoginstate] = useState(false)
+function App() {
+  const [isLoginState, setIsLoginstate] = useState(false);
+  const {gameInfo, monster,player, reset} = useContext(store)
+  const handleReset = () => {
+    setIsLoginstate(false);
+    reset(true);
+  }
   
-
-  useEffect(() => {
-    
-    
-  }, [])
   return (
     <>
-      {isLoginState ? <BoardComponent /> : <LoginComponent onchangeButton={()=> setIsLoginstate(true)} />}
-
-      
+      {isLoginState ? (
+        <BoardComponent
+          id={gameInfo.id}
+          currentTurn={gameInfo.currentTurn}
+          maxTurns={gameInfo.maxTurns}
+          turnsLeft={gameInfo.turnsLeft}
+          monsterHp={monster.hp}
+          playerHp={player.hp}
+          onReset={handleReset}
+        />
+      ) : (
+        <LoginComponent onchangeButton={() => setIsLoginstate(true)} />
+      )}
     </>
   );
 }
