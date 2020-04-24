7d5fa8b5a8188321676b9a45cf4ca9aa6e8486ae
Julio <pastor@indava.com>
2020-04-24 04:22:53 -0500

    style Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,32 @@
 import React, { useContext } from "react";
-import {store} from '../store'
+import { store } from "../store";
+import  {Card as  CardBoostrap, Button  }  from "react-bootstrap";
 
 const Card = ({ id, effect }) => {
-  const {setCardSelected} = useContext(store)
+  const { setCardSelected } = useContext(store);
   const onCardSelected = () => {
     setCardSelected(id);
-  }
-  
+  };
+
   return (
     <>
-      
-      <button onClick={ onCardSelected}> {effect } Secret Card</button>
+      <Button variant="outline-success" onClick={onCardSelected}>
+        {" "}        
+        <CardBoostrap
+          bg='info'          
+          text= "white"
+          style={{ width: "18rem" }}
+        >
+          <CardBoostrap.Img variant="top" src={`${process.env.PUBLIC_URL}/defaultCard.png `} />
+          <CardBoostrap.Body>
+            <CardBoostrap.Title>Select one </CardBoostrap.Title>
+            <CardBoostrap.Text>
+             
+            </CardBoostrap.Text>
+          </CardBoostrap.Body>
+        </CardBoostrap>
+        <br />
+      </Button>
     </>
   );
 };
